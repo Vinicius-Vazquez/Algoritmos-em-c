692ed271bd107a53078030c37e6aa623c6f0a860
Vinicius-Vazquez <vazquezvinicius50@gmail.com>
2026-03-17 18:03:39 -0300

    Criar calculadora_soma.c

---
+++ calculadora_soma.c
@@ -0,0 +1,38 @@
+/*
+Autor: Vinicius Vazquez
+Curso: Engenharia da Computação - CEFET
+Linguagem: C
+Descrição:
+Programa que solicita dois números inteiros ao usuário
+e exibe a soma entre eles.
+
+Data: 2026
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+int main()
+{
+    // Configura o idioma para português
+    setlocale(LC_ALL, "Portuguese");
+
+    // Declaração das variáveis
+    int num1, num2, soma;
+
+    // Entrada de dados
+    printf("Digite um número: ");
+    scanf("%d", &num1);
+
+    printf("Digite outro número: ");
+    scanf("%d", &num2);
+
+    // Cálculo da soma
+    soma = num1 + num2;
+
+    // Exibição do resultado
+    printf("A soma dos números é: %d\n", soma);
+
+    return 0;
+}
