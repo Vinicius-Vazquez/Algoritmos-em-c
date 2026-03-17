b357a6d6e82aa5cd816fd6ec21a7543f7f54d0be
Vinicius-Vazquez <vazquezvinicius50@gmail.com>
2026-03-17 18:05:06 -0300

    Criar quadrado_numero_real.c

---
+++ quadrado_numero_real.c
@@ -0,0 +1,35 @@
+/*
+Autor: Vinicius Vazquez
+Curso: Engenharia da Computação - CEFET
+Linguagem: C
+Descrição:
+Programa que solicita um número real ao usuário
+e calcula o quadrado desse número.
+
+Data: 2026
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+int main()
+{
+    // Configura o idioma para português
+    setlocale(LC_ALL, "Portuguese");
+
+    // Declaração das variáveis
+    float numeroReal, quadrado;
+
+    // Solicita um número real ao usuário
+    printf("Insira um número real: ");
+    scanf("%f", &numeroReal);
+
+    // Calcula o quadrado do número
+    quadrado = numeroReal * numeroReal;
+
+    // Exibe o resultado
+    printf("O quadrado do número é: %.2f\n", quadrado);
+
+    return 0;
+}
