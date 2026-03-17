f638880a0e4a6c2962374a17c5da98ef039da879
Vinicius-Vazquez <vazquezvinicius50@gmail.com>
2026-03-17 18:01:51 -0300

    Criar calculo_dobro_numero.c

---
+++ calculo_dobro_numero.c
@@ -0,0 +1,36 @@
+/*
+Autor: Vinicius Vazquez
+Curso: Engenharia da Computação - CEFET
+Linguagem: C
+Descrição:
+Programa que solicita um número inteiro ao usuário
+e calcula o dobro desse número.
+
+Data: 2026
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+int main()
+{
+    // Configura o idioma para português
+    setlocale(LC_ALL, "Portuguese");
+
+    // Declaração das variáveis
+    int num, dobro;
+
+    // Solicita um número ao usuário
+    printf("Insira um número inteiro: ");
+    scanf("%d", &num);
+
+    // Cálculo do dobro
+    dobro = num * 2;
+
+    // Exibe o resultado
+    printf("Número inserido: %d\n", num);
+    printf("Dobro do número: %d\n", dobro);
+
+    return 0;
+}
