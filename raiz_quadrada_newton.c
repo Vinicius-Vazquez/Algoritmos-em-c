0dc2341ee71aeab94848d9d4893d710a76d8be4d
Vinicius-Vazquez <vazquezvinicius50@gmail.com>
2026-03-17 18:08:20 -0300

    Criar raiz_quadrada_newton.c

---
+++ raiz_quadrada_newton.c
@@ -0,0 +1,44 @@
+/*
+Autor: Vinicius Vazquez
+Curso: Engenharia da Computação - CEFET
+Linguagem: C
+Descrição:
+Programa que calcula a raiz quadrada de um número utilizando
+o método de Newton (aproximação iterativa).
+
+O algoritmo começa com uma estimativa inicial e melhora
+progressivamente o resultado através de iterações.
+
+Data: 2026
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+int main()
+{
+    // Configura o idioma para português
+    setlocale(LC_ALL, "Portuguese");
+
+    // Declaração das variáveis
+    float num, aproximacao;
+
+    // Solicita um número ao usuário
+    printf("Digite um número: ");
+    scanf("%f", &num);
+
+    // Estimativa inicial
+    aproximacao = num;
+
+    // Método de Newton para calcular a raiz
+    for (int i = 0; i < 15; i++)
+    {
+        aproximacao = (aproximacao + (num / aproximacao)) / 2;
+    }
+
+    // Exibe o resultado aproximado
+    printf("A raiz aproximada do número é: %f\n", aproximacao);
+
+    return 0;
+}
